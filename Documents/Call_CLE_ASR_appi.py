c31cb487c3f98d109f08763ce6e2b1e3d15b04bc
Najm <najmayub167@gmail.com>
2025-06-04 00:57:24 +0500

    Update Call_CLE_ASR_appi.py

--- Documents/Call_CLE_ASR_appi.py
+++ Documents/Call_CLE_ASR_appi.py
@@ -2,7 +2,7 @@ import requests
 import time
 
 url = "https://api.cle.org.pk/asr"
-file_name = 'aa.wav'
+file_name = 'audio.wav'
 file = {'file': (file_name, open(file_name, 'rb'), 'audio/wav')}
 
 start_time = time.perf_counter()
@@ -10,6 +10,6 @@ response = requests.post(url, files=file)
 end_time = time.perf_counter()
 print(f"Time taken: {end_time - start_time:.6f} seconds")
 
-with open("aa.txt", "wb") as f:
+with open("audio.txt", "wb") as f:
     f.write(response.content)
 
